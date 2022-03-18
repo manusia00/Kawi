cebb9a5558a172b516a9ccb4028f7715bd16af22
dickyarya <dickyarya170101@gmail.com>
2022-03-18 10:46:41 +0700

    implement script playerMovement to char

---
+++ Assets/Script/playerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    [SerializeField]private float speed;
+    [SerializeField]private float jumpForce;
+
+    void Awake(){
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        if(Input.GetButtonDown("Jump")){
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+        }
+    }
+}
